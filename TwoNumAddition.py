e9e0eb6a61f7f22274efe458acad6ebc9bc5dfe6
16kajal7@gmail.com <16kajal7@gmail.com>
2025-09-09 19:46:53 +0530

    Third Commit of my python program project

---
+++ TwoNumAddition.py
@@ -0,0 +1,6 @@
+#This program sums up two numbers entered by user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+total = num1 + num2
+print ("The addition of two number you provided is: ", total)
